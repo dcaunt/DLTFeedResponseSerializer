c0a9e5e3732b43d80b30b291c44a5ba0ed7bb2d9
Dave Caunt <dcaunt@yahoo-inc.com>
2015-04-29 14:18:11 +0100

    Initial Commit

---
+++ DLTFeedResponseSerializer/DLTFeedResponseSerializer.h
@@ -0,0 +1,19 @@
+//
+//  DLTFeedResponseSerializer.h
+//  DLTFeedResponseSerializer
+//
+//  Created by David Caunt on 29/04/2015.
+//  Copyright (c) 2015 David Caunt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DLTFeedResponseSerializer.
+FOUNDATION_EXPORT double DLTFeedResponseSerializerVersionNumber;
+
+//! Project version string for DLTFeedResponseSerializer.
+FOUNDATION_EXPORT const unsigned char DLTFeedResponseSerializerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DLTFeedResponseSerializer/PublicHeader.h>
+
+
